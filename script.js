7a61e5b477447d92a60d3ddf32425ddf489c630b
manuelafrancomachado-del <manuela.franco.machado@escola.pr.gov.br>
2026-09-15 11:15:10 -0300

    arrumei o cod

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const botoesCurtir=document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botoesCurtir);{
+    let curtir=false;
+    botaoCurtir.addEventListener("click,curtir");
+    function curtir(){
+        const contador=botaoCurtir.querySelector("span");
+        if(curtir===false){
+            contador.textContent++;
+            curtiu=true;}
+            else{ con.textContent--;
+                curtir=false;
+            }
+    }
+});
